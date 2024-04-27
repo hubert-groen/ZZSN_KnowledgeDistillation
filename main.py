7cf84f8000678fdfb409c777585db33b16251752
itsMe <61231501+hubert-groen@users.noreply.github.com>
2024-04-27 13:26:34 +0200

    read index

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 from resnet import ResNet
 
+index = 9
 
 net = ResNet()
 net.load_parameters(path='saves/resnet__epoch_75.pth')
-net.train(save_dir='saves', num_epochs=75, batch_size=256, learning_rate=0.001, verbose=True)
-accuracy = net.test()
+net.train(save_dir='saves', read_index=index, num_epochs=75, batch_size=256, learning_rate=0.001, verbose=True)
+accuracy = net.test(index=9)
 print('Test accuracy: {}'.format(accuracy))
